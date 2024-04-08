45b52739be55a65b922523482438dc9a9e98ae09
mamontalvan <mamontalvan@gmail.com>
2024-04-08 17:43:28 -0500

    first commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'fIfCiog84qmlCPjRMa1LbeZ88jgNGL98';
+  private giphyServiceUrl: string = 'http://api.giphy.com/v1/gifs';
+  public gifsList: Gif[] = [];
+
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
+
+  public get tagsHistory(): string[] {
+    return [...this._tagsHistory];
+  }
+
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', '10');
+
+    this.http
+      .get<SearchResponse>(`${this.giphyServiceUrl}/search`, { params })
+      .subscribe((res) => {
+        this.gifsList = res.data;
+      });
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(
+        (t) => t.toLowerCase() !== tag
+      );
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(!this._tagsHistory) return;
+
+    this.searchTag(this._tagsHistory[0])
+  }
+}
